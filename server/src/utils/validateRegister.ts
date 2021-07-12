4e7e1b1d54ed31b0e9781bc898fc68ea899a07fa
AntwuanDixon <evanwchipman@gmail.com>
2021-07-12 12:40:45 -0700

    add nodemailer dependency, create utils for sending emails and register validation, add orm config file

---
+++ server/src/utils/validateRegister.ts
@@ -0,0 +1,41 @@
+import { UserNamePasswordInput } from "../resolvers/UserNamePasswordInput";
+
+export const validateRegister = (options: UserNamePasswordInput) => {
+  if (!options.email.includes("@")) {
+    return [
+      {
+        field: "email",
+        message: "invalid email",
+      },
+    ];
+  }
+
+  if (options.username.length <= 2) {
+    return [
+      {
+        field: "username",
+        message: "length must be greater than 2",
+      },
+    ];
+  }
+
+  if (options.username.includes("@")) {
+    return [
+      {
+        field: "username",
+        message: "cannot include an @",
+      },
+    ];
+  }
+
+  if (options.password.length <= 2) {
+    return [
+      {
+        field: "password",
+        message: "length must be greater than 2",
+      },
+    ];
+  }
+
+  return null;
+};
