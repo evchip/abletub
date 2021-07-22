467bc145d371c18920facc0558fa4f0a807b3f74
AntwuanDixon <evanwchipman@gmail.com>
2021-07-21 23:30:01 -0700

    fix: change production env variable to graphql, add favicon to _document file

--- web/src/pages/_document.js
+++ web/src/pages/_document.js
@@ -5,7 +5,11 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head >
+        <meta charSet="utf-8" />
+		    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        </Head >
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
