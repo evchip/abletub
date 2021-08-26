f3a6c2aa4c3d70d8203e2f7a888c82c2ef8687d9
AntwuanDixon <evanwchipman@gmail.com>
2021-08-25 17:08:25 -0700

    change TypeORM config synchronize to true for migrations

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,15 @@
-import { ApolloServer } from 'apollo-server-express';
-import connectRedis from 'connect-redis';
-import cors from 'cors';
-import express from 'express';
-import session from 'express-session';
-import Redis from 'ioredis';
+import { ApolloServer } from "apollo-server-express";
+import connectRedis from "connect-redis";
+import cors from "cors";
+import express from "express";
+import session from "express-session";
+import Redis from "ioredis";
 import path from "path";
 import "reflect-metadata";
 import "dotenv-safe/config";
-import { buildSchema } from 'type-graphql';
-import { createConnection } from 'typeorm';
-import { COOKIE_NAME, __prod__ } from './constants';
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { COOKIE_NAME, __prod__ } from "./constants";
 import { _Post as Post } from "./entities/Post";
 import { Updoot } from "./entities/Updoot";
 import { User } from "./entities/User";
@@ -18,80 +18,81 @@ import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 import { createUpvoteLoader } from "./utils/createUpvoteLoader";
 import { createUserLoader } from "./utils/createUserLoader";
-import { FileResolver } from './resolvers/file';
-
+import { FileResolver } from "./resolvers/file";
 
 const main = async () => {
-    const conn = await createConnection({
-        type: "postgres",
-        url: process.env.DATABASE_URL,
-        logging: true,
-        synchronize: true,
-        migrations: [path.join(__dirname, "./migrations/*")],
-        entities: [Post, User, Updoot]
-    });
+  const conn = await createConnection({
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    logging: true,
+    synchronize: true,
+    migrations: [path.join(__dirname, "./migrations/*")],
+    entities: [Post, User, Updoot],
+  });
 
-    
-    // await User.delete({});
-    await conn.runMigrations();
+  // RESET DB FUNCTIONS
+  // await User.delete({});
+  // await Post.delete({});
 
-    const app = express();
-    
-    // await Post.delete({});
-    const RedisStore = connectRedis(session);
-    const redis = new Redis(process.env.REDIS_URL);
-    app.set("trust proxy", 1);
-    app.use(
-        cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials: true
-    }));
+  await conn.runMigrations();
 
-    app.use(
-        session({
-            name: COOKIE_NAME,
-            store: new RedisStore({ 
-                client: redis,
-                disableTouch: true,
-            }),
-            cookie: {
-                maxAge: 1000 * 60 * 24 * 365 * 10, // 10 years
-                httpOnly: true,
-                sameSite: "lax", //csrf
-                secure: __prod__, // cookie only works in https
-                domain: __prod__ ? ".abletub.live" : undefined
-            },
-            saveUninitialized: false,
-            secret: process.env.SESSION_SECRET,
-            resave: false,
-        })
-    );
+  const app = express();
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver, UserResolver, FileResolver],
-            validate: false,
-        }),
-        context: ({ req, res }) => ({ 
-            req,
-            res,
-            redis,
-            userLoader: createUserLoader(),
-            updootLoader: createUpvoteLoader()
-        }),
-    });
+  const RedisStore = connectRedis(session);
+  const redis = new Redis(process.env.REDIS_URL);
+  app.set("trust proxy", 1);
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    })
+  );
 
-    apolloServer.applyMiddleware({ 
-        app, 
-        cors: false
-    });
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        client: redis,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", //csrf
+        secure: __prod__, // cookie only works in https
+        domain: __prod__ ? ".abletub.live" : undefined,
+      },
+      saveUninitialized: false,
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+    })
+  );
 
-    app.listen(parseInt(process.env.PORT), () => {
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver, FileResolver],
+      validate: false,
+    }),
+    context: ({ req, res }) => ({
+      req,
+      res,
+      redis,
+      userLoader: createUserLoader(),
+      updootLoader: createUpvoteLoader(),
+    }),
+    introspection: true,
+  });
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
+
+  app.listen(parseInt(process.env.PORT), () => {
     console.log("Node server started");
-
-    });
+  });
 };
 
 main().catch((err) => {
-    console.log('err::::', err)
-})
+  console.log("err::::", err);
+});
