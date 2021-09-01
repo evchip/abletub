b1730461035b32cddf05d1cd2b170b8d03328474
AntwuanDixon <evanwchipman@gmail.com>
2021-08-31 18:32:49 -0700

    add audio URL as props passed to Howler Audio

--- web/src/components/PlayPauseAudio.jsx
+++ web/src/components/PlayPauseAudio.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import ReactHowler from 'react-howler'
-import {Button} from "@chakra-ui/react"
+import {Button, Box } from "@chakra-ui/react"
 
 class OnlyPlayPauseButton extends React.Component {
   constructor (props) {
     super(props)
+    
+    console.log(props)
 
     this.state = {
-      playing: false
+      playing: false,
+      audioURL: props.audioURL,
+      getStatus: () => {console.log('this state', this.state)}
     }
+
     this.handlePlay = this.handlePlay.bind(this)
     this.handlePause = this.handlePause.bind(this)
   }
+
 
   handlePlay () {
     this.setState({
@@ -29,7 +35,7 @@ class OnlyPlayPauseButton extends React.Component {
     return (
       <div>
         <ReactHowler
-          src={['/audioFiles/Flumeish.mp3', 'sound.mp3']}
+          src={[this.props.audioURL, 'sound.mp3']}
           playing={this.state.playing}
         />
         <Button onClick={this.handlePlay} colorScheme="teal" variant="solid" m="auto" ml={8}>Play</Button>
