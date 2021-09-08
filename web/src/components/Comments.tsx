a52695267dc3624af97314b329049cac70b4f1e8
AntwuanDixon <evanwchipman@gmail.com>
2021-09-07 21:59:11 -0700

    render comment in UI after posting

--- web/src/components/Comments.tsx
+++ web/src/components/Comments.tsx
@@ -1,36 +1,45 @@
+import { ChevronDownIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
   Flex,
   Heading,
   HStack,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Text,
   VStack,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
-import { format } from "timeago.js";
+import router from "next/router";
+import React, { useEffect, useState } from "react";
 import { useCommentsQuery } from "../generated/graphql";
+import Comment from "./Comment";
 
 interface CommentsProps {
   postId: number;
+  newComment: any;
 }
 
-export const Comments: React.FC<CommentsProps> = ({ postId }) => {
+export const Comments: React.FC<CommentsProps> = ({ postId, newComment }) => {
   const [variables, setVariables] = useState({
     postId,
-    limit: 10,
+    limit: 50,
     cursor: null as null | string,
   });
+
   const [{ data, error, fetching }] = useCommentsQuery({
     variables,
   });
-  console.log("comments data", data, "postId", postId);
+
   if ((!fetching && !data) || (!fetching && !data!.comments.comments[0])) {
     return (
-      <div>
-        <Text>There are no comments yet. Would you do the honors?</Text>
+      <Box display="flex" justifyContent="center" mt={10}>
+        <Text color="white">There are no comments yet. Would you do the honors?</Text>
         <div>{error?.message}</div>
-      </div>
+      </Box>
     );
   }
 
@@ -39,61 +48,81 @@ export const Comments: React.FC<CommentsProps> = ({ postId }) => {
       {!data && fetching ? (
         <div>Loading...</div>
       ) : (
-        <VStack align="center" flexWrap="wrap" width="100%" my={10}>
-          {data!.comments.comments.map((c, i) =>
-            !c ? null : (
-              <Box
-                key={i}
-                p={5}
-                display="flex"
-                flexDirection="column"
-                shadow="md"
+        <>
+          <Flex justifyContent="flex-end">
+            <Menu>
+              <MenuButton
+                fontSize={{ base: "12px", md: "14px", lg: "16px" }}
+                as={Button}
+                rightIcon={<ChevronDownIcon />}
+                px={4}
+                py={2}
+                color="black"
+                transition="all 0.2s"
+                borderRadius="md"
                 borderWidth="1px"
-                direction="column"
-                bgColor="gray.400"
-                borderRadius="5"
-                width="100%"
+                _hover={{ bg: "gray.400" }}
+                _expanded={{ bg: "blue.400" }}
+                _focus={{ boxShadow: "outline" }}
               >
-                <Flex
-                  justifyContent="space-between"
-                  mt="1"
-                  width="100%"
-                  direction="column"
-                >
-                  <Flex align="left" width="80%" m="auto">
-                    <Heading fontSize="md">{c.text}</Heading>
-                  </Flex>
-                  <Flex
-                    direction="row"
-                    justifyContent="flex-end"
-                    width="100%"
-                    mt={2}
-                  >
-                    <Text fontSize="md" mr={2}>
-                      {c.creator.username}
-                    </Text>
-                    <Text fontSize="md" mr={2}>
-                      {format(c.createdAt)}
-                    </Text>
-                  </Flex>
-                </Flex>
-              </Box>
-            )
-          )}
-        </VStack>
+                sort by
+              </MenuButton>
+              <MenuList bgColor="black">
+                <MenuItem onClick={() => {
+                setVariables({
+                  postId,
+                  limit: variables.limit,
+                  cursor: "DESC"
+                });
+            }} _hover={{ bg: "gray.400" }}>New</MenuItem>
+                <MenuItem onClick={() => {
+                setVariables({
+                  postId,
+                  limit: variables.limit,
+                  cursor:
+                    "ASC"
+                });
+            }} _hover={{ bg: "gray.400" }}>
+                  Old
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+          <VStack align="center" flexWrap="wrap" width="100%" my={10}>
+            {newComment.text ? (
+              <Comment
+                i={newComment.i}
+                text={newComment.text}
+                creator={newComment.creator}
+                createdAt={newComment.createdAt}
+              />
+            ) : null}
+            {data!.comments.comments.map((c, i) =>
+              !c ? null : (
+                <Comment
+                  i={i}
+                  text={c.text}
+                  creator={c.creator}
+                  createdAt={c.createdAt}
+                />
+              )
+            )}
+          </VStack>
+        </>
       )}
+
       {data && data.comments.hasMore ? (
         <Flex>
           <Button
             isLoading={fetching}
             onClick={() => {
-              setVariables({
-                postId,
-                limit: variables.limit,
-                cursor:
-                  data.comments.comments[data.comments.comments.length - 1]
-                    .createdAt,
-              });
+                setVariables({
+                  postId,
+                  limit: variables.limit,
+                  cursor:
+                    data.comments.comments[data.comments.comments.length - 1]
+                      .createdAt,
+                });
             }}
             colorScheme="teal"
             variant="solid"
--- web/src/components/CreateComment.tsx
+++ web/src/components/CreateComment.tsx
@@ -1,55 +1,60 @@
-import { Box, Button, Flex } from '@chakra-ui/react';
-import { Formik, Form } from 'formik';
-import { useCreateCommentMutation } from 'generated/graphql';
-import router from 'next/router';
-import createPost from 'pages/create-post';
-import React, { ReactElement } from 'react'
-import { InputField } from './InputField';
-import { Layout } from './Layout';
-
+import { Box, Button, Flex } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import { useCreateCommentMutation } from "generated/graphql";
+import router from "next/router";
+import React, { ReactElement } from "react";
+import { InputField } from "./InputField";
 
 interface Props {
-    postId: number
+  postId: number;
+  getNewComment(postId: number, values: { text: string }): void;
 }
 
-function CreateComment({postId}: Props): ReactElement {
+function CreateComment({ postId, getNewComment }: Props): ReactElement {
 
-    const [, createComment] = useCreateCommentMutation()
-    return (
-        <Box variant="small" display="flex" direction="row" width="100%">
-            <Formik 
-            initialValues={{ text: ''}} 
-            onSubmit={ async (values) => {
-                
-                const {error } = await createComment({postId, input: values})
-                console.log("error", error)
-                if (error) {
-                    console.log("error", error)
-                } else {
-                    router.push(`/post/${postId}`);
-                }
-            }}>
-            {({ isSubmitting }) => (
-                <Flex direction="row" width="100%">
-                <Form >
-                    <Box mt={4} width="500px">
-                        <InputField textarea name="text" placeholder="Tell them what you think" label="Comment" />
-                    </Box>
-                    <Button 
-                        mt={4} 
-                        type='submit' 
-                        isLoading={isSubmitting} 
-                        colorScheme="teal"
-                    >
-                        Comment
-                    </Button>
-                </Form>
-                </Flex>
-                
-            )}
-            </Formik>
-        </Box>
-    )
+  const [, createComment] = useCreateCommentMutation();
+
+  return (
+    <Box variant="small" display="flex" direction="row" width="100%">
+      <Formik
+        initialValues={{ text: "" }}
+        onSubmit={async (values) => {
+          const { error } = await createComment({ postId, input: values });
+          
+          if (error) {
+            console.log("error", error);
+          } else {
+            getNewComment(postId, values)
+
+            // router.push(`/post/${postId}`);
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Flex direction="row" width="100%">
+            <Form>
+              <Box mt={4} width="500px">
+                <InputField
+                  textarea
+                  name="text"
+                  placeholder="Tell them what you think"
+                  label="Comment"
+                />
+              </Box>
+              <Button
+                mt={4}
+                type="submit"
+                isLoading={isSubmitting}
+                colorScheme="teal"
+              >
+                Comment
+              </Button>
+            </Form>
+          </Flex>
+        )}
+      </Formik>
+    </Box>
+  );
 }
 
-export default CreateComment
+export default CreateComment;
