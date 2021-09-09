b4b4589c529f3b70b135d33e8a44f187ec8d5dd2
AntwuanDixon <evanwchipman@gmail.com>
2021-09-08 23:46:06 -0700

    remove audiobar

--- web/src/components/AudioBar.tsx
+++ web/src/components/AudioBar.tsx
@@ -1,17 +1,48 @@
-import React from 'react'
+import { Box, Button } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 
-interface Props {
-    
-}
+const padTime = time => {
+    return String(time).length === 1 ? `0${time}` : `${time}`;
+  };
+  
+  const format = time => {
+    // Convert seconds into minutes and take the whole part
+    const minutes = Math.floor(time / 60);
+  
+    // Get the seconds left after converting minutes
+    const seconds = time % 60;
+  
+    //Return combined values as string in format mm:ss
+    return `${minutes}:${padTime(seconds)}`;
+  };
 
-const AudioFooter = (props: Props) => {
+interface Props {}
 
-    return (
-        <Box display="flex" className="meter-container">
-            <meter id="timer-gauge" value="10" min="0" max="100"></meter>
-            <p className='counter' id='timer-seconds-elapsed'></p>
-        </Box>
-    )
-}
+const AudioBar = (props: Props) => {
 
-export default AudioFooter;
+  const [counter, setCounter] = React.useState(0);
+  React.useEffect(() => {
+    let timer;
+    if (counter < 120) {
+      timer = setTimeout(() => setCounter(c => c + 1), 1000);
+    }
+
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [counter]);
+
+  return (
+    <Box display="flex" className="meter-container" width="80%" justifyContent="space-between">
+        <Box width="20%">{counter === 0 ? "Time over" : <div>{format(counter)}</div>}</Box>
+        <Box width="50%">
+            <meter id="timer-gauge" value={counter} min="0" max="100" style={{ transform: "rotate(-90deg)", height:"300px", width:"200px"}}></meter>
+            </Box>
+      
+    </Box>
+  );
+};
+
+export default AudioBar;
