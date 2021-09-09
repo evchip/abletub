3731ebbb9071f9d7ace7f8a52e5099df9f40e58e
AntwuanDixon <evanwchipman@gmail.com>
2021-09-08 23:54:10 -0700

    aimport global.css file

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 import theme from '../theme';
 import { extendTheme } from "@chakra-ui/react"
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: any) {
   return (
