56d0fab380a8236efd3ce490df6e90f3eeed0543
AntwuanDixon <evanwchipman@gmail.com>
2021-09-08 23:50:24 -0700

    change color and font style to UI theme

--- web/src/pages/post/[id].tsx
+++ web/src/pages/post/[id].tsx
@@ -1,26 +1,29 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import { Comments } from "components/Comments";
 import CreateComment from "components/CreateComment";
 import S3Image from "components/Image";
-import WaveForm from "components/WaveForm";
-import WaveFormFC from "components/WaveFormFC";
-import { useCommentsQuery, useGetNewCommentQuery } from "generated/graphql";
 import { withUrqlClient } from "next-urql";
 import React, { useEffect, useState } from "react";
 import { EditDeletePostBtns } from "../../components/EditDeletePostBtns";
 import { Layout } from "../../components/Layout";
 import { createUrqlClient } from "../../utils/createUrqlClient";
 import { useGetPostFromUrl } from "../../utils/useGetPostfromUrl";
-import Comment from '../../components/Comment'
+import { format } from "timeago.js";
+import { UpvoteSection } from "components/UpvoteSection";
 
 const Post = ({}) => {
   const [{ data, error, fetching }] = useGetPostFromUrl();
-  const [newComment, setNewComment] = useState({})
+  const [newComment, setNewComment] = useState({});
 
   const getNewComment = async (postId: number, values: { text: string }) => {
-    const now = new Date()
-    setNewComment({text: values.text, creator: {username: "you"}, createdAt: now, i: 0})
-  }
+    const now = new Date();
+    setNewComment({
+      text: values.text,
+      creator: { username: "you" },
+      createdAt: now,
+      i: 0,
+    });
+  };
 
   if (fetching) {
     return (
@@ -43,36 +46,67 @@ const Post = ({}) => {
   }
   return (
     <Layout>
-        <Flex
-          p={5}
-          mt="0"
-          shadow="md"
-          borderWidth="1px"
-          width="100%"
-          flexDirection="row"
-          justifyContent="space-between"
-          bgColor="blackAlpha.400"
-        >
-          <Box width="70%">
-            <Heading mb={4}>{data.post.title}</Heading>
-              <Box ml="5px">{data.post.creator.username}</Box>
-                <WaveFormFC audioURL={data.post.audioFileName} />
-              <Box ml="5px">{data.post.text}</Box>
-            </Box>
-            <Box ml={5}>
-          <EditDeletePostBtns
-            id={data.post.id}
-            creatorId={data.post.creator.id}
-          />
+      <Flex
+        p={5}
+        mt="0"
+        shadow="md"
+        borderWidth="1px"
+        width="100%"
+        flexDirection="row"
+        justifyContent="space-between"
+        bgColor="blackAlpha.400"
+        borderBottomRadius="30px"
+        borderColor="pink.200"
+        borderTop="none"
+      >
+        <Box width="70%">
+          <Heading mb={4} color="white">
+            {data.post.title}
+          </Heading>
+          <Box ml="5px">
+            <Heading mb={4} size="md" color="white">
+              {data.post.creator.username}
+            </Heading>
+          </Box>
+          <Flex
+            justifyContent="space-between"
+            mt="3"
+            ml="1"
+            width="100%"
+            direction="column"
+            height="60%"
+          >
+            <Flex align="left" width="80%">
+              <Text color="white" fontSize="md" mr={2}>
+                {data.post.text}
+              </Text>
+            </Flex>
+            <Flex
+              direction="row"
+              justifyContent="space-between"
+              alignItems="end"
+              width="100%"
+              mt={2}
+            >
+              <EditDeletePostBtns
+                id={data.post.id}
+                creatorId={data.post.creator.id}
+              />
+              <Text color="white" fontSize="md" mr={2}>
+                published {format(data.post.createdAt)}
+              </Text>
+            </Flex>
+          </Flex>
         </Box>
-            <Box>
-              {data.post.imageFileName !== null ? (
-                <S3Image post={data.post} />
-              ) : null}
-            </Box>
-        </Flex>
-        <CreateComment postId={data.post.id} getNewComment={getNewComment}/>
-        <Comments postId={data.post.id} newComment={newComment}/>
+        <Box display="flex" direction="row" >
+        <UpvoteSection post={data.post} />
+          {data.post.imageFileName !== null ? (
+            <S3Image post={data.post} />
+          ) : null}
+        </Box>
+      </Flex>
+      <CreateComment postId={data.post.id} getNewComment={getNewComment} />
+      <Comments postId={data.post.id} newComment={newComment} />
     </Layout>
   );
 };
