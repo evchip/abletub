65ef4f3a180a08673ddff131c7e41bec6f3cc949
AntwuanDixon <evanwchipman@gmail.com>
2021-09-11 01:33:37 -0700

    create audio footer for global track play

--- web/src/components/AudioFooter.tsx
+++ web/src/components/AudioFooter.tsx
@@ -1,30 +1,76 @@
-import { useGetAudioFileQuery } from 'generated/graphql';
-import { withUrqlClient } from 'next-urql';
-import React from 'react'
-import { createUrqlClient } from 'utils/createUrqlClient';
-import WaveForm from './WaveForm'
-import axios from 'axios';
-import WaveFormFC from './WaveFormFC';
+import { useGetAudioFileQuery } from "generated/graphql";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { PlayButton, Timer, Progress } from "react-soundplayer/components";
+import { withCustomAudio } from "react-soundplayer/addons";
+import { Flex, Text } from "@chakra-ui/react";
 
 interface Props {
-    
+  streamURL: string;
+  trackTitle: string;
+  artist: string;
+  playPause: boolean;
+  trackId: number;
+  togglePausePlayOnPost(postId: number): void;
 }
 
-const AudioFooter = (props: Props) => {
-    const [{ data, error, fetching }] = useGetAudioFileQuery()
+const AudioPlayer = withCustomAudio((props) => {
+    console.log(props)
+  const { trackTitle, artist, playPause, playing, soundCloudAudio, track, trackId, togglePausePlayOnPost } = props;
 
 
-    if (!data) {
-        return null
+  useEffect(() => {
+    if (playPause) {
+        soundCloudAudio.play();
     } else {
-        console.log(data.getAudioFile)
+        soundCloudAudio.pause();
     }
-    
+  }, [playPause])
+
+  const handleChange = () => {
+    if (playing) {
+        soundCloudAudio.pause();
+    } else {
+        soundCloudAudio.play();
+    }
+    togglePausePlayOnPost(trackId)
+  }
+  
+
+  return (
+    <Flex zIndex={60} position="sticky" bottom={0} bg="black" p={4} alignContent="center" justifyContent="space-between">
+        <Flex alignItems="center">
+      <PlayButton style={{ width: "20px", height: "20px", marginLeft:"2rem" }} {...props} onTogglePlay={() => handleChange()}/>
+      <Text ml={8}>{trackTitle}</Text>
+      <Text ml={8}>{artist}</Text>
+      </Flex>
+      <Flex alignItems="center" width="50%">
+      <Progress
+        className="progress-bar"
+        innerClassName="rounded-left"
+        value={(props.currentTime / props.duration) * 100 || 0}
+        {...props}
+      />
+      </Flex>
+    <Timer style={{ marginRight:"2rem" }} {...props} />
+    </Flex>
+  );
+});
+
+class AudioFooter extends React.Component<Props> {
+
+  render() {
     return (
-        <div>
-            <WaveForm audioURL={data.getAudioFile}/>
-        </div>
-    )
+      <AudioPlayer
+        streamUrl={this.props.streamURL}
+        trackTitle={this.props.trackTitle}
+        artist={this.props.artist}
+        playPause={this.props.playPause}
+        playing={this.props.playPause}
+        togglePausePlayOnPost={this.props.togglePausePlayOnPost}
+      />
+    );
+  }
 }
 
 export default AudioFooter;
