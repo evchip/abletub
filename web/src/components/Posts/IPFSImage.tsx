84b9e783897e67cfa266718a11efb1654390e869
AntwuanDixon <evanwchipman@gmail.com>
2021-09-24 11:44:26 -0700

    remove unnecessary imports and format JS

--- web/src/components/Posts/IPFSImage.tsx
+++ web/src/components/Posts/IPFSImage.tsx
@@ -1,69 +1,66 @@
-
-import { _Post } from 'generated/graphql';
-import React, { ReactElement, useEffect, useState } from 'react'
-import { Web3Storage } from "web3.storage";
-import { Box, Image } from "@chakra-ui/react";
+import { _Post } from "generated/graphql";
+import React, { ReactElement, useEffect, useState } from "react";
+import { Box, Image, Skeleton } from "@chakra-ui/react";
 import PlayPauseAudioFC from "./PlayPauseAudioFC";
+import { IPFSRequestHandler } from "../../utils/fetchIPFSData"
 
 interface Props {
-    post: Pick<_Post, "id" | "createdAt" | "updatedAt" | "title" | "points" | "audioFileName" | "imageFileName" | "text" | "voteStatus"> | undefined;
-
+  post:
+    | Pick<
+        _Post,
+        | "id"
+        | "createdAt"
+        | "updatedAt"
+        | "title"
+        | "points"
+        | "audioFileName"
+        | "imageFileName"
+        | "text"
+        | "voteStatus"
+      >
+    | undefined;
 }
 
-function IPFSImage({post}: Props): ReactElement {
-    const [CID, setCID] = useState("")
+function IPFSImage({ post }: Props): ReactElement {
+  const [imageCID, setImageCID] = useState("");
+  const defaultImageURL =
+    "https://bafybeie43scgedoyeiizc7cssktdjendathgqk4oz5cmblulcdubm5qebi.ipfs.dweb.link/";
 
-    const requestHandler = async () => {
-        return await retrieve(post?.imageFileName!)
-    }
+  useEffect(() => {
+    (async () => {
+      const CID = (await IPFSRequestHandler(defaultImageURL, post!.imageFileName)) as string;
+      setImageCID(CID);
+    })();
+  }, [imageCID]);
 
-    const getAccessToken = () => {
-        return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGY5RGIxZDcwNzI2NjBCNjM4YjI0QWIwQjFGOEQ5OGFGZWNhZTlERUYiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2MzIxNjk0NTI3NjcsIm5hbWUiOiJhYmxldHViIn0.fFhf0CfKqDOST6pADwgrffCz4P2AU5_FwmLOcMcxws4" as string;
-      }
-
-    const makeStorageClient = () => {
-        const client = new Web3Storage({ token: getAccessToken() })
-        return client
-      }
-
-    const retrieve = async (cid: string) => {
-        const client = makeStorageClient()
-        const res = await client.get(cid)
-        console.log(`Got a response! [${res.status}] ${res.statusText}`)
-        if (!res.ok) {
-          throw new Error(`failed to get ${cid}`)
-        }
-        const files = await res?.files()
-        const CIDConcat = `https://${files[0].cid}.ipfs.dweb.link/`
-        setCID(CIDConcat)
-      }
-
-    useEffect(() => {
-        requestHandler()
-    }, [CID])
-    return (
-        <Box>
-            <div className="port-cont-hov tile scale-anm">
-                <Image
-                    boxSize={["40", "72"]}
-                    borderRadius="2rem"
-                    className="portfolio-img"
-                    src={CID}
-                    alt=""
-                    objectFit="cover"
-                />
-                <Box className="port-img-overlay" borderRadius="2rem">
-                    <div className="port-text">
-                    { post.audioFileName ? ( 
-                    <PlayPauseAudioFC
-                        post={post}
-                    />) : (null)}
-                    
-                    </div>
-                </Box>
-            </div>
-        </Box>
-    )
+  return (
+    <Box>
+      <div className="port-cont-hov tile scale-anm">
+        <Skeleton
+          startColor="pink.500"
+          endColor="purple.500"
+          height={["40", "72"]}
+          borderRadius="2em"
+          fadeDuration={6}
+          isLoaded={imageCID !== "" ? true : false}
+        >
+          <Image
+            boxSize={["40", "72"]}
+            borderRadius="2rem"
+            className="portfolio-img"
+            src={imageCID}
+            alt=""
+            objectFit="cover"
+          />
+          <Box className="port-img-overlay" borderRadius="2rem">
+            <div className="port-text">
+            {post?.audioFileName !== "" ? <PlayPauseAudioFC post={post} /> : null}
+          </div>
+          </Box>
+        </Skeleton>
+      </div>
+    </Box>
+  );
 }
 
-export default IPFSImage
+export default IPFSImage;
