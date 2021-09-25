4d32389f159d5b8d895c009bac3bb6ba4f2555d5
AntwuanDixon <evanwchipman@gmail.com>
2021-09-25 11:33:12 -0700

    add global types for track context

--- web/all.d.ts
+++ web/all.d.ts
@@ -1,5 +1,18 @@
 declare module "next-apollo";
 
 interface Window {
-    webkitAudioContext: typeof AudioContext
-  }
+  webkitAudioContext: typeof AudioContext;
+}
+
+interface Track {
+  title: string;
+  artist: string;
+  streamUrl: string;
+  trackId: number;
+  isPlaying: boolean;
+}
+
+type ContextType = {
+  track: Track;
+  setTrack: (track: Track) => void;
+};
