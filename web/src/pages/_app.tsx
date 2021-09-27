6e4bcd3b87aa490f93db2e00841237411e632499
AntwuanDixon <evanwchipman@gmail.com>
2021-09-27 16:30:38 -0700

    add apollo provider wrapper

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,31 +1,32 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import theme from '../theme';
-import '../styles/globals.css'
-import  TrackContext, {defaultTrack}  from '../utils/trackContext';
-import { useState } from 'react';
-import FooterWrapper from '../components/Footer/FooterWrapper'
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../theme";
+import "../styles/globals.css";
+import TrackContext from "../utils/trackContext";
+import { useState } from "react";
+import FooterWrapper from "../components/Footer/FooterWrapper";
+
+
 
 function MyApp({ Component, pageProps }: any) {
-
   const defaultTrack = {
     title: "",
     artist: "",
     streamUrl: "",
     trackId: 0,
     isPlaying: false,
-  }
+  };
 
-  const [track, setTrack] = useState(defaultTrack)
+  const [track, setTrack] = useState(defaultTrack);
 
   return (
 
-    <ChakraProvider resetCSS theme={theme}>
-      <TrackContext.Provider value={{track, setTrack}}>
-        <Component {...pageProps} />
-        <FooterWrapper />
-      </TrackContext.Provider >
-    </ChakraProvider>
-  )
+      <ChakraProvider resetCSS theme={theme}>
+        <TrackContext.Provider value={{ track, setTrack }}>
+          <Component {...pageProps} />
+          <FooterWrapper />
+        </TrackContext.Provider>
+      </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
