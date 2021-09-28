e0b8d13fa7d486a31e7a0c3933be9521c1ae097c
AntwuanDixon <evanwchipman@gmail.com>
2021-09-28 13:27:49 -0700

    add update cache resolver to use apollo syntax

--- web/src/components/Comments/CreateComment.tsx
+++ web/src/components/Comments/CreateComment.tsx
@@ -5,6 +5,8 @@ import { withUrqlClient } from "next-urql";
 import router from "next/router";
 import React, { ReactElement } from "react";
 import { createUrqlClient } from "utils/createUrqlClient";
+import { useGetPostFromUrl } from "utils/useGetPostfromUrl";
+import { withApollo } from "utils/withApollo";
 import { InputField } from "../FormFields/InputField";
 
 interface Props {
@@ -12,45 +14,54 @@ interface Props {
 }
 
 function CreateComment({ postId }: Props): ReactElement {
-
-  const [, createComment] = useCreateCommentMutation();
+  const [createComment] = useCreateCommentMutation();
 
   return (
     <Box variant="small" display="flex" direction="row" width="100%">
       <Formik
         initialValues={{ text: "" }}
-        onSubmit={async (values, {resetForm}) => {
-          const { error } = await createComment({ postId, input: values });
-          
-          if (error) {
-            console.log("error", error);
+        onSubmit={async (values, { resetForm }) => {
+          const { errors } = await createComment({
+            variables: { postId, input: values },
+            update: (cache) => {
+              cache.evict({ fieldName: "comments" });
+            },
+          });
+
+          if (errors) {
+            console.log("error", errors);
           } else {
-            resetForm({ values: {text: ''} })
+            resetForm({ values: { text: "" } });
             router.push(`/post/${postId}`);
           }
         }}
       >
         {({ isSubmitting }) => (
           <Flex width="100%">
-            <Form style={{ width: "100%"}}>
-              <Flex w="100%" direction={["column", "row"]} alignItems="center" justifyContent="space-between">
-              <Flex mt={4} width="85%" justifyContent="flex-start" >
-                <InputField
-                  size=""
-                  textarea
-                  name="text"
-                  placeholder="tell them what you think"
-                  label=""
-                />
-              </Flex>
-              <Button
-                mt={4}
-                type="submit"
-                isLoading={isSubmitting}
-                colorScheme="pink"
+            <Form style={{ width: "100%" }}>
+              <Flex
+                w="100%"
+                direction={["column", "row"]}
+                alignItems="center"
+                justifyContent="space-between"
               >
-                comment
-              </Button>
+                <Flex mt={4} width="85%" justifyContent="flex-start">
+                  <InputField
+                    size=""
+                    textarea
+                    name="text"
+                    placeholder="tell them what you think"
+                    label=""
+                  />
+                </Flex>
+                <Button
+                  mt={4}
+                  type="submit"
+                  isLoading={isSubmitting}
+                  colorScheme="pink"
+                >
+                  comment
+                </Button>
               </Flex>
             </Form>
           </Flex>
@@ -60,4 +71,4 @@ function CreateComment({ postId }: Props): ReactElement {
   );
 }
 
-export default withUrqlClient(createUrqlClient, { ssr: true })(CreateComment);
+export default CreateComment;
