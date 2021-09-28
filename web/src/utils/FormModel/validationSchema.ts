3fc9d73c3eb223b69a3ff3b87910571eef71aecc
AntwuanDixon <evanwchipman@gmail.com>
2021-09-28 13:20:18 -0700

    update validation schema based on new arguments passed to form

--- web/src/utils/FormModel/validationSchema.ts
+++ web/src/utils/FormModel/validationSchema.ts
@@ -14,12 +14,11 @@ export default [
     [audio.name]: Yup.string().required(`${audio.requiredErrorMsg}`),
     [audio.name]: Yup.mixed()
       .test('audioFileSize', `${audio.invalidErrorMsg}`, (val) => {
-        return val[0].size <= 50000000
+        return val.target.files[0].size <= 50000000
       })
   }),
   Yup.object().shape({
     [trackName.name]: Yup.string().required(`${trackName.requiredErrorMsg}`),
-    [trackDescription.name]: Yup.string().required(`${trackDescription.requiredErrorMsg}`),
-    [image.name]: Yup.string().required(`${image.requiredErrorMsg}`)
+    [trackDescription.name]: Yup.string().required(`${trackDescription.requiredErrorMsg}`)
   })
 ];
