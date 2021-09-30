4845f307e1a1d64f0a6104e9efc561a64b8d4a5e
AntwuanDixon <evanwchipman@gmail.com>
2021-09-30 01:51:39 -0700

    add site title to head section

--- web/src/pages/_document.js
+++ web/src/pages/_document.js
@@ -5,6 +5,8 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head >
+        <title>a b l e t u b</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" type="image/x-icon" href="/baby-tub.ico" />
         </Head >
         <body>
